ee0de37dd2ae6d20b3d67b7671d4a0646c0ddd45
Denis Manriquez <denis.manriquez@est.fi.uncoma.edu.ar>
2023-04-05 15:16:26 -0300

    Ejercicio extra1 con Clase Cancion Hechos

--- src/EjercicioExtraPOO/Ejercicio1Extra.java
+++ src/EjercicioExtraPOO/Ejercicio1Extra.java
@@ -5,7 +5,15 @@
  */
 package EjercicioExtraPOO;
 
-/**
+import Entidad.Cancion;
+
+/**Desarrollar una clase Cancion con los 
+ * siguientes atributos: título y autor. Se deberá 
+ * definir además dos constructores: uno vacío que 
+ * inicializa el título y el autor a cadenas vacías y
+ * otro que reciba como parámetros el título y el autor
+ * de la canción. Se deberán además definir los métodos 
+ * getters y setters correspondientes.
  *
  * @author denis
  */
@@ -15,7 +23,9 @@ public class Ejercicio1Extra {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Cancion disquetera = new Cancion();
+        System.out.println(disquetera.toString());
+        
     }
     
 }
