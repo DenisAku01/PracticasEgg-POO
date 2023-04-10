ef4c7942684adcc6cf2d08f8a5ef3968ea6adf66
Denis Manriquez <denis.manriquez@est.fi.uncoma.edu.ar>
2023-04-10 16:39:46 -0300

    EjercicioExtra5 Agregado

---
+++ src/EjercicioExtraPOO/EjercicioExtra5.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjercicioExtraPOO;
+
+/**
+ *
+ * @author denis
+ */
+public class EjercicioExtra5 {
+
+    /**Crea una clase "Empleado" que tenga atributos como "nombre", "edad" y "salario".
+     * Luego, crea un método "calcular_aumento" que calcule el aumento salarial de un
+     * empleado en función de su edad y salario actual. El aumento salarial debe ser
+     * del 10% si el empleado tiene más de 30 años o del 5% si tiene menos de 30 años.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
